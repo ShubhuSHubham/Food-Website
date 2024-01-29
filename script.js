29a688729738f9fd31801750f3558e5aff8fa301
ShubhuSHubham <shubhushubham66@gmail.com>
2024-01-29 22:04:36 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("myButton");
+
+//Add a click event listner to the button
+button.addEventListener('click', function () {
+
+    alert("Your respond have been submited....");
+})
